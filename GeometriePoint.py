7a333a0ae28552fb9f0ac646812ad6cf3ccb5fe1
Martin C <martin.couture@uqtr.ca>
2021-01-31 16:46:44 -0500

    Create GeometriePoint.py

---
+++ GeometriePoint.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import arcpy
+arcpy.env.workspace = "C:/Temp/donnees.gdb"
+
+point = arcpy.Point(-71.22734082248334, 46.83023275891952)
+sr = arcpy.SpatialReference(4326) #WGS84
+
+geomPoint = arcpy.PointGeometry(point, sr)
+arcpy.CopyFeatures_management(geomPoint, "points")
+
+
+
+
+
+
+
+
+
+
