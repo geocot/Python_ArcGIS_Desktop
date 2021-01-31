78009a4f921d6e4c65643a88468feead5ec940fe
Martin C <martin.couture@uqtr.ca>
2021-01-31 16:49:19 -0500

    Update GeometriePoint.py

--- GeometriePoint.py
+++ GeometriePoint.py
@@ -7,6 +7,7 @@ point = arcpy.Point(-71.22734082248334, 46.83023275891952)
 sr = arcpy.SpatialReference(4326) #WGS84
 
 geomPoint = arcpy.PointGeometry(point, sr)
+#On pourait utiliser une liste de points géométriques
 arcpy.CopyFeatures_management(geomPoint, "points")
 
 
