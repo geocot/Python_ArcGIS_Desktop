3a1f9f40ca6962f043e5a1db6d59ea8edbc7c93f
Martin C <martin.couture@uqtr.ca>
2021-01-31 22:21:29 -0500

    Update mapping_Liste_Couches.py

--- mapping_Liste_Couches.py
+++ mapping_Liste_Couches.py
@@ -7,7 +7,7 @@ mxd = arcpy.mapping.MapDocument("C:/temp/amerique.mxd")
 for lyr in arcpy.mapping.ListLayers(mxd):
     try:
         print("Le nom est {} et la source est {}".format(lyr.name, lyr.dataSource))
-    except:
+    except:#Ce n'est pas toutes les couches qui ont une information de source disponible via dataSource
         pass
 
 
