82396d514b6bcbeca97a20425dd4b8ad151713ea
Martin C <martin.couture@uqtr.ca>
2021-01-31 22:19:40 -0500

    Update mapping_Liste_Couches.py

--- mapping_Liste_Couches.py
+++ mapping_Liste_Couches.py
@@ -5,6 +5,9 @@ import arcpy
 mxd = arcpy.mapping.MapDocument("C:/temp/amerique.mxd")
 
 for lyr in arcpy.mapping.ListLayers(mxd):
-    print(lyr.name)
+    try:
+        print("Le nom est {} et la source est {}".format(lyr.name, lyr.dataSource))
+    except:
+        pass
 
 
