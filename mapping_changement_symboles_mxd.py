ff3e33c9b8ac623c3a9d60bcb353600c8f241db4
Martin C <martin.couture@uqtr.ca>
2021-02-01 15:20:23 -0500

    Create mapping_changement_symboles_mxd.py

---
+++ mapping_changement_symboles_mxd.py
@@ -0,0 +1,11 @@
+import arcpy
+mxd = arcpy.mapping.MapDocument("C:/Temp/Donnees_Gouv/prj2.mxd")
+lyr = arcpy.mapping.ListLayers(mxd)[1]
+print(lyr)
+print(lyr.symbologyType)
+if lyr.symbologyType == "UNIQUE_VALUES":
+    lyr.symbology.valueField = "MUS_CO_DES"
+    lyr.symbology.addAllValues()
+arcpy.RefreshActiveView()
+arcpy.RefreshTOC()
+mxd.saveACopy("C:/Temp/Donnees_Gouv/prj3.mxd")
