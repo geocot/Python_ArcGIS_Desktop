3a11fd7fea9349caff05a596e1a659b3ff880780
Martin C <martin.couture@uqtr.ca>
2021-01-31 21:25:09 -0500

    Update export_MXD_vers_PDF.py

--- export_MXD_vers_PDF.py
+++ export_MXD_vers_PDF.py
@@ -6,3 +6,19 @@ mxd = arcpy.mapping.MapDocument("C:/temp/amerique.mxd")
 arcpy.mapping.ExportToPDF(mxd, "C:/temp/amerique.pdf")
 
 
+"""
+Avec une liste de fichier MXD.
+
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+import arcpy
+
+arcpy.env.workspace = "c:/temp"
+
+# Copy each file with a .csv extension to a dBASE file
+for fichier_MXD in arcpy.ListFiles("*.mxd"):
+    mxd = arcpy.mapping.MapDocument(fichier_MXD)
+    arcpy.mapping.ExportToPDF(mxd, fichier_MXD + ".pdf")
+
+
+"""
